e57d883f392a16be4cff490ff3b0e13f087ff1e8
Whxismou1 <melouc00@estudiantes.unileon.es>
2023-11-24 12:36:12 +0100

    Subida commit final(Pendiente refactor y comentarios)

--- VueloSim.c
+++ VueloSim.c
@@ -5,54 +5,206 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+void tecnicSig();
+void assistSig();
+void respSig();
+void errorPid(pid_t pid);
+void assistantMessage(int numAsistantPasseng, pid_t *numAsistentesArr, int j);
+bool exitsOverbook(int overBook);
+void printFinalFree(pid_t *numAsistentesArr, int numPassengers, int exitFinalCode);
+int generarRandom(int num1, int num2);
 
 int main(int argc, char const *argv[])
 {
-    /* code */
 
-    int numAsistentes = atoi(argv[1]);
+    if(argc != 2){
+        printf("ERROR: Num args innvalid!\n");
+        exit(1);
+    }
 
-    //Creacion tecnico del avion
-    __pid_t pidTecnico, pidEncargado;
+    // Creacion tecnico del avion
+    pid_t pidTecnico;
 
     pidTecnico = fork();
+    errorPid(pidTecnico);
 
-    if (pidTecnico < 0)
+    if (pidTecnico == 0)
     {
-        //Entramos en el caso de error 
-        printf("Error en el proceso tecnico\n");
-        exit(EXIT_FAILURE);
-
-    }else if(pidTecnico == 0){
-        //Codigo del proceso tecnico
-        printf("Proceso tecnico\n");
-        exit(EXIT_SUCCESS);
+        // Entramos en el hijo tecnico
+        if (signal(SIGUSR1, tecnicSig) == SIG_ERR)
+        {
+            perror("Señal del técnico.");
+        }
+        pause();
+        
+        srand(getpid());
+        sleep(generarRandom(3, 6));
+        exit(generarRandom(0, 1));
     }
-    
+    else
+    {
+        // Creamos el encargado
+        pid_t pidEncargado;
+        pidEncargado = fork();
+        errorPid(pidEncargado);
+        if (pidEncargado == 0)
+        {
+            if (signal(SIGUSR1, respSig) == SIG_ERR)
+            {
+                perror("Señal del encargado.");
+            }
+            pause();
+            
+            srand(getpid());
+            int numOver = generarRandom(0, 1);
+            sleep(3);
+            exit(numOver);
+        }
+        else
+        {
+            int numAsistentes = atoi(argv[1]);
+            pid_t *numAsistentesArr;
+            numAsistentesArr = (int *)malloc(numAsistentes * sizeof(pid_t));
 
-    //Creacion del encargado
-    pidEncargado = fork();
+            for (int i = 0; i < numAsistentes; i++)
+            {
+                pid_t pidAsist = fork();
+                errorPid(pidAsist);
 
-    if(pidEncargado < 0){
-        printf("Error en el proceso encargado\n");
-        //Si falla el proceso encargado matamos el proceso tecnico
-        kill(pidTecnico, SIGKILL);
-        //Salimos con codigo de error
-        exit(EXIT_FAILURE);
-    }else{
-        //Codigo del proceso encargado
-        printf("El proceso encargado es %d\n", pidEncargado);
-        exit(EXIT_SUCCESS);
+                if (pidAsist == 0)
+                {
+                    if (signal(SIGUSR2, assistSig) == SIG_ERR)
+                    {
+                        perror("Señal del asistente.");
+                    }
+                    pause();
+                    srand(getpid());
+                    
+                    sleep(generarRandom(3, 6));
+                    int numPasseng = generarRandom(20, 30);
+                    exit(numPasseng);
+                }
+                else
+                {
+                    numAsistentesArr[i] = pidAsist;
+                }
+            }
+
+            // Estamos en coordinador
+
+            sleep(2);
+            kill(pidTecnico, SIGUSR1);
+            int exitStatus;
+            wait(&exitStatus);
+            int numPassengers = 0;
+            int exitCodeStatus = WEXITSTATUS(exitStatus);
+            if (exitCodeStatus != 0)
+            {
+                // Vuelo viable
+                printf("Vuelo viable");
+                kill(pidEncargado, SIGUSR1);
+                wait(&exitStatus);
+                int overB = WEXITSTATUS(exitStatus);
+
+                if (overB != 1)
+                {
+                    printf("No overbooking");
+                }
+                else
+                {
+                    printf("Overbooking");
+                }
+
+                for (int i = 0; i < numAsistentes; i++)
+                {
+                    kill(numAsistentesArr[i], SIGUSR2);
+                }
+
+                for (int j = 0; j < numAsistentes; j++)
+                {
+                    wait(&exitStatus);
+                    int numAsistantPasseng = WEXITSTATUS(exitStatus);
+                    numPassengers += numAsistantPasseng;
+
+                    assistantMessage(numAsistantPasseng, numAsistentesArr, j);
+                }
+
+                if (exitsOverbook(overB))
+                {
+                    numPassengers -= 10;
+                }
+
+                printFinalFree(numAsistentesArr, numPassengers, 0);
+            }
+            else
+            {
+                for (int i = 0; i < numAsistentes; i++)
+                {
+                    kill(numAsistentesArr[i], SIGKILL);
+                }
+                kill(pidEncargado, SIGKILL);
+                kill(pidTecnico, SIGKILL);
+                printFinalFree(numAsistentesArr, numPassengers, 1);
+            }
+        }
     }
 
-
-
-
-
-
-
-
-
-    printf("Hola mundo mi parametro es %d\n", numAsistentes);
     return 0;
+}
+
+void tecnicSig()
+{
+    printf("Technician checking flight\n");
+}
+void assistSig()
+{
+    printf("Assistant %d boarding passengers\n", getpid());
+}
+
+void respSig()
+{
+    printf("Manager checking overbooking\n");
+}
+
+void errorPid(pid_t pid)
+{
+    if (pid < 0)
+    {
+        printf("ERROR: fork()");
+        exit(1);
+    }
+}
+
+void assistantMessage(int numAsistantPasseng, pid_t *numAsistentesArr, int j)
+{
+    printf("Assistant %d has boarded %d passengers.\n", numAsistentesArr[j], numAsistantPasseng);
+}
+
+bool exitsOverbook(int overBook)
+{
+    if (overBook == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+void printFinalFree(pid_t *numAsistentesArr, int numPassengers, int exitFinalCode)
+{
+    if (exitFinalCode == 0)
+    {
+        printf("There are a total of %d passengers\n", numPassengers);
+    }
+    else
+    {
+        printf("The flight is not feasible.\n");
+    }
+    free(numAsistentesArr);
+}
+
+int generarRandom(int num1, int num2)
+{
+    return rand() % (num2 - num1 + 1) + num1;
 }
